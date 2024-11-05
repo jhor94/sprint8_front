505f179f5db01586787da2786b3682e39b07456c
Jhorman Cadena Valero <jhor@MacBook-Air-de-Jhorman.local>
2024-11-05 13:55:15 +0100

    se realiza llamada y conexion de api con el back y se configura botones del crud

--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -2,35 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Crud } from '../../interfaces/crud';
 import { RouterModule } from '@angular/router';
 import { PersonasService } from '../../services/personas/personas.service';
+import { ProgressBarComponent } from "../../shared/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ProgressBarComponent],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.scss'
 })
-export class PersonasComponent  implements OnInit{
+export class PersonasComponent implements OnInit {
   personaServicio = inject(PersonasService)
-  listaCrud: Crud[] = [{
-    id: 1,
-    nombre: 'Jhorman',
-    apellido:'Cadena',
-    email:'jhor@gmail.com',
-    telefono:'1234567',
-    localizacion:'Barcelona',
-    hobby:'Bailar',
-  }]
-  
+  listaCrud: Crud[] = []
+  loading: Boolean = false
+
   ngOnInit(): void {
-    
+    this.getListaCrud()
   }
-  
-  
-  getListaCrud(){
-    this.personaServicio.getCrud().subscribe((data: any) => {
+
+  getListaCrud() {
+    this.loading = true
+
+    this.personaServicio.getCrud().subscribe((data: Crud[]) => {
       this.listaCrud = data
       console.log(this.listaCrud)
-  })
+      this.loading = false;
+    })
   }
+
+  borrarPersona(id: number) {
+    console.log(id);
+    this.personaServicio.deleteCrud(id).subscribe(()=> {
+      this.getListaCrud()
+    })
+  }
+
 }
--- src/app/services/personas/personas.service.ts
+++ src/app/services/personas/personas.service.ts
@@ -2,18 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Crud } from '../../interfaces/crud';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonasService {
-  private backUrl = "http://localhost:4000/productos"
-  httpCliente = inject(HttpClient)
+  private backUrl: string;
+  private apiUrl: string;
+  
 
-  getCrud(): Observable<Crud>{
-    return this.httpCliente.get<Crud>(`${this.backUrl}`)
+  constructor (private httpCliente: HttpClient) { 
+    this.backUrl = environment.endpoint;
+    this.apiUrl = 'api/personas/'
   }
 
+  getCrud(): Observable<Crud[]>{
+    return this.httpCliente.get<Crud[]>(`${this.backUrl}${this.apiUrl}`)
+  }
 
-  constructor() { }
+  deleteCrud(id: number):Observable<void>{
+    return this.httpCliente.delete<void>(`${this.backUrl}${this.apiUrl}${id}`)
+  }
+
 }
