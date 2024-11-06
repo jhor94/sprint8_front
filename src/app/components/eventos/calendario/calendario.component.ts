f760e79d223249297c43fa49d860661c97dd1990
Jhorman Cadena Valero <jhor@MacBook-Air-de-Jhorman.local>
2024-11-07 00:32:38 +0100

    se realiza el componente eventos, se inserta el calendario y se hace logica para creacion de eventos de manera local

--- src/app/components/eventos/calendario/calendario.component.ts
+++ src/app/components/eventos/calendario/calendario.component.ts
@@ -1,23 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions, EventInput } from '@fullcalendar/core/index.js';
+import { CalendarOptions, EventInput, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core/index.js';
+import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import esLocale from '@fullcalendar/core/locales/es';
-
+import listPlugin from "@fullcalendar/list"
 
 
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [FullCalendarModule, CommonModule],
+  imports: [FullCalendarModule, CommonModule, RouterOutlet],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CalendarioComponent implements OnInit {
   title: string = 'Calendario de eventos';
@@ -27,30 +25,77 @@ export class CalendarioComponent implements OnInit {
     { title: 'Evento del ocio', date: '2024-11-12' }
   ]
 
-  calendarOptions: CalendarOptions = {
+  calendarVisible = signal<boolean>(true)
+  calendarOptions = signal<CalendarOptions>({
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin, 
+      timeGridPlugin,
+      listPlugin
+    ],
+    headerToolbar:{
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
     initialView: 'dayGridMonth',
-    weekends:false,
-    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-    locale: esLocale,
-    editable:false,
-    dateClick: (arg) => this.handleDateClick(arg),
-    events: this.events
+    weekends: true,
+    editable:true,
+    selectable:true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateClick.bind(this), //agrega eventos
+    eventClick: this.handleEventClick.bind(this), //elimina eventos
+    eventsSet: this.handleEvents.bind(this) //actualiza eventos
+  });
+
+  currentEvents = signal<EventApi[]>([])
+
+  constructor(private changeDetector: ChangeDetectorRef){}
+
+  handleCalendarToogle(){
+    this.calendarVisible.update((bool) => !bool);
   }
 
-  eventTitle: string = '';
-  eventDate: string = '';
-  eventDescription: string = '';
+  handleWeekendsToggle(){
+    this.calendarOptions.update((options) => ({
+      ...options,
+      weekends: !options.weekends
+    }))
+  }
 
+  handleDateClick(selectInfo:DateSelectArg) {
+    const title = prompt ('Introduce el titulo del evento');
+    const calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect();
+
+    if(title){
+      calendarApi.addEvent({
+      id:String(Date.now()),
+      title,
+      start:selectInfo.startStr,
+      end:selectInfo.endStr,
+      allDay: selectInfo.allDay
+    });
+  }
+}
+
+handleEventClick(clickInfo: EventClickArg) {
+  if(confirm(`¿Seguro quiere eliminar el evento ${clickInfo.event.title}`)){
+    clickInfo.event.remove()
+  }
+
+}
+
+handleEvents(){
+
+}
 
   ngOnInit(): void {
 
   }
 
-  handleDateClick(arg: any) {
-    this.eventDate = arg.dateStr
-  }
+ 
 
-  eventClick() {
-
-  }
 }
