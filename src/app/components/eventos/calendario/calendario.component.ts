205ad8bae6d780ecda417e0eda3ac29e366c03df
Jhorman Cadena Valero <jhor@MacBook-Air-de-Jhorman.local>
2024-11-07 16:19:20 +0100

    se realiza metodos para actualizar y eliminar eventos y se añade modal

--- src/app/components/eventos/calendario/calendario.component.ts
+++ src/app/components/eventos/calendario/calendario.component.ts
@@ -10,6 +10,9 @@ import listPlugin from "@fullcalendar/list"
 import { CalendarioService } from '../../../services/calendarios/calendario.service';
 import { Eventos } from '../../../interfaces/eventos';
 
+//bootstrap
+import * as bootstrap from 'bootstrap'
+
 
 
 @Component({
@@ -22,121 +25,144 @@ import { Eventos } from '../../../interfaces/eventos';
 export class CalendarioComponent implements OnInit {
   title: string = 'Calendario de eventos';
 
-  eventos: EventInput[] = [
-    { title: 'Evento del trabajo', date: '2024-11-10' },
-    { title: 'Evento del ocio', date: '2024-11-12' }
-  ]
+  eventos: EventInput[] = []
 
   calendarVisible = signal<boolean>(true)
   calendarOptions = signal<CalendarOptions>({
     plugins: [
       interactionPlugin,
-      dayGridPlugin, 
+      dayGridPlugin,
       timeGridPlugin,
       listPlugin
     ],
-    headerToolbar:{
+    headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
     },
     initialView: 'dayGridMonth',
     weekends: true,
-    editable:true,
-    selectable:true,
+    editable: true,
+    selectable: true,
     selectMirror: true,
     dayMaxEvents: true,
     select: this.handleDateClick.bind(this), //agrega eventos
     eventClick: this.handleEventClick.bind(this), //elimina eventos
-    eventsSet: this.handleEvents.bind(this) //actualiza eventos
   });
 
   currentEvents = signal<EventApi[]>([])
 
-  constructor(private changeDetector: ChangeDetectorRef, private calendarioService: CalendarioService){}
+  constructor(private changeDetector: ChangeDetectorRef, private calendarioService: CalendarioService) { }
 
   ngOnInit(): void {
     this.loadEventos()
-
   }
 
-    loadEventos(){
-
-      this.calendarioService.getEventos().subscribe((events) => {
-        
-        this.eventos = events.map((evento) => ({
-          id: evento.id?.toString(),
-          title: evento.titulo,
-          start: evento.inicio,
-          end: evento.fin,
-          description: evento.descripcion,
-
-    }));
-    this.calendarOptions.update(options =>({
-      ...options,
-      events:this.eventos
-    }))
-    })
-    }
-  
-  handleCalendarToogle(){
-    this.calendarVisible.update((bool) => !bool);
-  }
-
-  handleWeekendsToggle(){
-    this.calendarOptions.update((options) => ({
-      ...options,
-      weekends: !options.weekends
-    }))
-  }
-
-  handleDateClick(selectInfo:DateSelectArg) {
-    const title = prompt ('Introduce el titulo del evento');
-    const description = prompt ('Introduce una pequeña descripcion del evento') ?? ''; //asigna campo vacio en el caso que sea nulo
-    const finEventStr = prompt ('Introduce la fecha de finalizacion en formado (yyy-mm-dd)') ?? ''; //asigna campo vacio en el caso que sea nulo
-    const finEvent = new Date(finEventStr)
-    const calendarApi = selectInfo.view.calendar
-    calendarApi.unselect();
-
-    if(title){
-     const nuevoEvento: Eventos = {
-      titulo: title,
-      inicio: new Date(selectInfo.startStr),
-      fin: finEvent,
-      descripcion: description,
-      creado_en: new Date(),
-      actualizado_en: new Date()
-     };
-     this.calendarioService.guardarEvento(nuevoEvento).subscribe((evento) => {
-      this.loadEventos()
-      calendarApi.addEvent({
+  //actualizar todos los eventos del calendario
+  loadEventos() {
+    this.calendarioService.getEventos().subscribe((events) => {
+      this.eventos = events.map((evento) => ({
         id: evento.id?.toString(),
         title: evento.titulo,
         start: evento.inicio,
         end: evento.fin,
         description: evento.descripcion,
+
+      }));
+      this.calendarOptions.update(options => ({
+        ...options,
+        events: this.eventos
+      }))
+    })
+  }
+
+  handleCalendarToogle() {// mostrar o ocultar el calendario
+    this.calendarVisible.update((bool) => !bool);
+  }
+
+  //headel para crear
+  handleDateClick(selectInfo: DateSelectArg) {
+    const title = prompt('Introduce el titulo del evento');
+    const description = prompt('Introduce una pequeña descripcion del evento') ?? ''; //asigna campo vacio en el caso que sea nulo
+    const finEventStr = prompt('Introduce la fecha de finalizacion en formado (yyy-mm-dd)') ?? ''; //asigna campo vacio en el caso que sea nulo
+    const finEvent = new Date(finEventStr)
+    const calendarApi = selectInfo.view.calendar
+    calendarApi.unselect();
+
+    if (title) {
+      const nuevoEvento: Eventos = {
+        titulo: title,
+        inicio: new Date(selectInfo.startStr),
+        fin: finEvent,
+        descripcion: description,
+        creado_en: new Date(),
+        actualizado_en: new Date()
+      };
+      this.calendarioService.guardarEvento(nuevoEvento).subscribe((evento) => {
+        this.loadEventos()
+        calendarApi.addEvent({
+          id: evento.id?.toString(),
+          title: evento.titulo,
+          start: evento.inicio,
+          end: evento.fin,
+          description: evento.descripcion,
+        })
       })
-     })
-     
+
+    }
+
   }
 
+  eventoSeleccionado: EventClickArg | null = null
 
-
-}
-
-handleEventClick(clickInfo: EventClickArg) {
-  if(confirm(`¿Seguro quiere eliminar el evento ${clickInfo.event.title}`)){
-    clickInfo.event.remove()
+  //evento ya creados
+  handleEventClick(clickInfo: EventClickArg) {
+    this.eventoSeleccionado = clickInfo
+    const modal = new bootstrap.Modal(document.getElementById('eventoModal')!);
+    modal.show()
+    document.getElementById('eventoTitulo')!.innerHTML = clickInfo.event.title
   }
+  // headel para borrar
+  borrarEvento() {
 
+    const eventoId = Number(this.eventoSeleccionado?.event.id)
+
+    if (eventoId) {
+      if (confirm(`¿Seguro quiere eliminar el evento ${this.eventoSeleccionado?.event.title}`)) {
+        this.calendarioService.deleteEvento(eventoId).subscribe(() => {
+          this.eventoSeleccionado?.event.remove()
+          this.eventoSeleccionado = null
+        })
+      }
+    }
+    bootstrap.Modal.getInstance(document.getElementById('eventoModal')!)?.hide()
+  }
+  //headel para actualizar
+  actualizarEvento() {
+    if (this.eventoSeleccionado) {
+      const newTitle = prompt('Actualiza el titulo del evento', this.eventoSeleccionado?.event.title) ?? this.eventoSeleccionado?.event.title
+      const newDescription = prompt('Actualizac la descripción', this.eventoSeleccionado?.event.extendedProps['description']) ?? this.eventoSeleccionado?.event.extendedProps['description']
+
+      const originalCreationDate = this.eventoSeleccionado?.event.extendedProps['creado_en'] || this.eventoSeleccionado?.event.start;
+
+      const UpdateEvent: Eventos = {
+        id: Number(this.eventoSeleccionado.event.id),
+        titulo: newTitle,
+        inicio: this.eventoSeleccionado.event.start!,
+        fin: this.eventoSeleccionado.event.end!,
+        descripcion: newDescription,
+        creado_en: originalCreationDate,
+        actualizado_en: new Date()
+      }
+
+      this.calendarioService.updateEvento(UpdateEvent.id!, UpdateEvent).subscribe(() => {// paso el id por parametro y pongo !para evitar el null
+        this.eventoSeleccionado!.event.setProp('title', newTitle);
+        this.eventoSeleccionado!.event.setProp('description', newDescription);
+        this.eventoSeleccionado = null
+      })
+      bootstrap.Modal.getInstance(document.getElementById('eventoModal')!)?.hide()
+
+    }
+  }
 }
 
-handleEvents(){
-
-}
-
-
-
- 
-
-}
